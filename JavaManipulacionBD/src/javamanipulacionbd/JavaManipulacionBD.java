faf8f1d4c2421b5588853a9b1deb22583f475038
Brayan <brayanbhdzl@micorreo.upp.edu.mx>
2021-02-09 13:39:41 -0600

    Subiendo la clase principal

---
+++ JavaManipulacionBD/src/javamanipulacionbd/JavaManipulacionBD.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javamanipulacionbd;
+
+/**
+ *
+ * @author Braya
+ */
+public class JavaManipulacionBD {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
