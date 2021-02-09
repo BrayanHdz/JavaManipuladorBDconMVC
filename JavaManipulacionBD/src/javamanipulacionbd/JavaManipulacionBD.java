bb130debc54e4cd59827459a2f6df6879501ec0d
BrayanHdz <78815429+BrayanHdz@users.noreply.github.com>
2021-02-09 13:41:18 -0600

    agrengando linea de codigo Hola mundo

--- JavaManipulacionBD/src/javamanipulacionbd/JavaManipulacionBD.java
+++ JavaManipulacionBD/src/javamanipulacionbd/JavaManipulacionBD.java
@@ -15,7 +15,7 @@ public class JavaManipulacionBD {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hola mundo del GIT");
     }
     
 }
