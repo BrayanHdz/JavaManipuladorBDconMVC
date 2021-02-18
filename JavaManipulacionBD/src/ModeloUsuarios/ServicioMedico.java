6925022bb86a92a40295f6e22411ca445bf1567e
Brayan <brayanbhdzl@micorreo.upp.edu.mx>
2021-02-17 20:53:13 -0600

    "Modificacion de la clase ServicioMedico"

--- JavaManipulacionBD/src/ModeloUsuarios/ServicioMedico.java
+++ JavaManipulacionBD/src/ModeloUsuarios/ServicioMedico.java
@@ -1,16 +1,57 @@
-
 import ModeloUsuarios.Usuario;
-import java.util.*;
 
-/**
- * 
- */
+
 public class ServicioMedico extends Usuario {
 
-    /**
-     * Default constructor
-     */
-    public ServicioMedico() {
+    
+    private String numeroSeguroSocial;
+    private String fechaNacimiento;
+    private boolean sexo;
+    private String telefonoEmergencia;
+    
+    public ServicioMedico(String nss, String fN, boolean sexo, String telEmg) {
+        this.numeroSeguroSocial = nss;
+        this.fechaNacimiento = fN;
+        this.sexo = sexo;
+        this.telefonoEmergencia = telEmg;
+        
+    }
+
+    public void setNumeroSeguroSocial(String nss) {
+        this.numeroSeguroSocial = nss;
+    }
+    public String getNumeroSeguroSocial() {
+        return "";
+    }
+
+    public void setFechaNacimiento(String fN) {
+        this.fechaNacimiento = fN;
+    }
+
+    public String getFechaNacimiento() {
+        return "";
+    }
+    public void setSexo(boolean sexo) {
+        this.sexo = sexo;
+    }
+
+    public boolean getSexo() {
+        return false;
+    }
+    
+    public void setTelefonoEmergencia(String telEmg) {
+        this.telefonoEmergencia = telEmg;
+    }
+
+    public String getTelefonoEmergencia() {
+        return "";
+    }
+
+    public void consulta() {
+    }
+
+    public String resetarMedicamento() {
+        return "";
     }
 
 }
