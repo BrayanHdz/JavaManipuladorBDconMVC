2892254f2a7581ffa2e393233beb799e087b81ae
Brayan <brayanbhdzl@micorreo.upp.edu.mx>
2021-02-18 16:38:03 -0600

    "Agregando metodos y atributos a Caja y Mantenimiento"

--- JavaManipulacionBD/src/ModeloUsuarios/Caja.java
+++ JavaManipulacionBD/src/ModeloUsuarios/Caja.java
@@ -2,15 +2,47 @@
 import ModeloUsuarios.Usuario;
 import java.util.*;
 
-/**
- * 
- */
 public class Caja extends Usuario {
 
-    /**
-     * Default constructor
-     */
-    public Caja() {
+    private float cantidadTotal;
+    private String fechaRetiro;
+    private String fechaDeposito;
+    
+    public Caja(float cantidad, String fecha1, String fecha2) {
+        this.cantidadTotal = cantidad;
+        this.fechaRetiro = fecha1;
+        this.fechaDeposito = fecha2;
     }
 
+    public float getCantidadTotal() {
+        return cantidadTotal;
+    }
+
+    public void setCantidadTotal(float cantidadTotal) {
+        this.cantidadTotal = cantidadTotal;
+    }
+
+    public String getFechaRetiro() {
+        return fechaRetiro;
+    }
+
+    public void setFechaRetiro(String fechaRetiro) {
+        this.fechaRetiro = fechaRetiro;
+    }
+
+    public String getFechaDeposito() {
+        return fechaDeposito;
+    }
+
+    public void setFechaDeposito(String fechaDeposito) {
+        this.fechaDeposito = fechaDeposito;
+    }
+    
+    public void retirar(float cantidad){
+        this.cantidadTotal -= cantidad;
+    }
+
+    public void depositar(float cantidad){
+        this.cantidadTotal += cantidad;
+    }
 }
--- JavaManipulacionBD/src/ModeloUsuarios/Mantenimiento.java
+++ JavaManipulacionBD/src/ModeloUsuarios/Mantenimiento.java
@@ -2,15 +2,54 @@
 import ModeloUsuarios.Usuario;
 import java.util.*;
 
-/**
- * 
- */
 public class Mantenimiento extends Usuario {
 
-    /**
-     * Default constructor
-     */
-    public Mantenimiento() {
+    private float presupuesto;
+    private String fechaUltimoMantenimiento;
+    private String turno;
+    private String area;
+    
+    public Mantenimiento(float presupuesto, String fecha, String turno, String area) {
+        this.presupuesto = presupuesto;
+        this.fechaUltimoMantenimiento = fecha;
+        this.turno = turno;
+        this.area = area;
+    }
+
+    public float getPresupuesto() {
+        return presupuesto;
+    }
+
+    public void setPresupuesto(float presupuesto) {
+        this.presupuesto = presupuesto;
+    }
+
+    public String getFechaUltimoMantenimiento() {
+        return fechaUltimoMantenimiento;
+    }
+
+    public void setFechaUltimoMantenimiento(String fechaUltimoMantenimiento) {
+        this.fechaUltimoMantenimiento = fechaUltimoMantenimiento;
+    }
+
+    public String getTurno() {
+        return turno;
+    }
+
+    public void setTurno(String turno) {
+        this.turno = turno;
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+    
+    public void reparar(){
+    
     }
 
 }
