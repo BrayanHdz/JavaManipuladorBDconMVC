46bb294cb767a6cdc1686c024354814512f93b14
Nestor-DS <78669277+Nestor-DS@users.noreply.github.com>
2021-02-17 21:28:33 -0600

    "Se agregaron atributos y metodos"

--- JavaManipulacionBD/src/ModeloUsuarios/Profesores.java
+++ JavaManipulacionBD/src/ModeloUsuarios/Profesores.java
@@ -3,6 +3,21 @@ import ModeloUsuarios.Usuario;
 import java.util.*;
 
 /**
+  
+ /
+public class Profesores extends Usuario {
+
+    /**
+     * Default constructor
+     /
+    public Profesores() {
+    }
+
+}*/
+
+import java.util.*;
+
+/**
  * 
  */
 public class Profesores extends Usuario {
@@ -11,6 +26,264 @@ public class Profesores extends Usuario {
      * Default constructor
      */
     public Profesores() {
+    }
+
+    /**
+     * 
+     */
+    private String cedula;
+
+    /**
+     * 
+     */
+    private String nombre;
+
+    /**
+     * 
+     */
+    private String correoElectronico;
+
+    /**
+     * 
+     */
+    private String telefono;
+
+    /**
+     * 
+     */
+    private String user;
+
+    /**
+     * 
+     */
+    private String psw;
+
+    /**
+     * 
+     */
+    public void Profesores() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    void Profesores() {
+        // TODO implement here
+    }
+
+    /**
+     * @return
+     */
+    public void setCedula() {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @return
+     */
+    public void setNombre() {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @return
+     */
+    public String getNombre() {
+        // TODO implement here
+        return "";
+    }
+
+    /**
+     * @return
+     */
+    public void setCorreoElectronico() {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @return
+     */
+    public String getCorreoElectronico() {
+        // TODO implement here
+        return "";
+    }
+
+    /**
+     * @return
+     */
+    public void setTelefono() {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @return
+     */
+    public int getTelefono() {
+        // TODO implement here
+        return 0;
+    }
+
+    /**
+     * @return
+     */
+    public void setUser() {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @return
+     */
+    public String getUser() {
+        // TODO implement here
+        return "";
+    }
+
+    /**
+     * @return
+     */
+    protected void setPsw() {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @return
+     */
+    protected String getPsw() {
+        // TODO implement here
+        return "";
+    }
+
+    /**
+     * 
+     */
+    public void Profesores() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    void Profesores() {
+        // TODO implement here
+    }
+
+    /**
+     * @return
+     */
+    public void setCedula() {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @return
+     */
+    public String getCedula() {
+        // TODO implement here
+        return "";
+    }
+
+    /**
+     * @return
+     */
+    public void setNombre() {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @return
+     */
+    public String getNombre() {
+        // TODO implement here
+        return "";
+    }
+
+    /**
+     * @return
+     */
+    public void setCorreoElectronico() {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @return
+     */
+    public String getCorreoElectronico() {
+        // TODO implement here
+        return "";
+    }
+
+    /**
+     * @return
+     */
+    public void setTelefono() {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @return
+     */
+    public String getTelefono() {
+        // TODO implement here
+        return "";
+    }
+
+    /**
+     * @return
+     */
+    public void setUser() {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @return
+     */
+    public String getUser() {
+        // TODO implement here
+        return "";
+    }
+
+    /**
+     * @return
+     */
+    public void setPsw() {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @return
+     */
+    public String getPsw() {
+        // TODO implement here
+        return "";
+    }
+
+    /**
+     * @return
+     */
+    public void subirCalificaciones() {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @return
+     */
+    public void modificarCalificaciones() {
+        // TODO implement here
+        return null;
     }
 
 }
