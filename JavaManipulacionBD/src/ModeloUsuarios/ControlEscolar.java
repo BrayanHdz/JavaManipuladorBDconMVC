a6c841f83673e3d73cbc63788544e94995b6f9b5
SebastianMM <78822949+MR-EAZY@users.noreply.github.com>
2021-02-17 20:06:57 -0600

    Actualice atributos y metodos (Sebastian Montiel Moreno)
    agregué atributos, getters, setters y metodos

--- JavaManipulacionBD/src/ModeloUsuarios/ControlEscolar.java
+++ JavaManipulacionBD/src/ModeloUsuarios/ControlEscolar.java
@@ -9,15 +9,48 @@ public class ControlEscolar extends Usuario {
 private String matricula;
 private String area;
 private Boolean empleadoEscuela;
-    
 
-
-}
-  
-    public ControlEscolar() 
+    public ControlEscolar(String matricula, String area, Boolean empleadoEscuela) {
+        this.matricula = matricula;
+        this.area = area;
+        this.empleadoEscuela = empleadoEscuela;
     }
 
-public void AgregarAlumno(){
-}
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+
+    public Boolean getEmpleadoEscuela() {
+        return empleadoEscuela;
+    }
+
+    public void setEmpleadoEscuela(Boolean empleadoEscuela) {
+        this.empleadoEscuela = empleadoEscuela;
+    }
+   
+    
+    
+    public ControlEscolar() {
+    }
+    
+    public void agregarAlumno(){
+        
+    }
+    
+    public void eliminarAlumno(){
+        
+    }
 
 }
